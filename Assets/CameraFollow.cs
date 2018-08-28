6fa2f342b7d8f99ed70cdb6ccb00af01e46fbcac
Marco Pompa <marcopompa7@gmail.com>
2018-08-28 18:34:02 +0200

    Steering mechanic testing

--- Assets/CameraFollow.cs
+++ Assets/CameraFollow.cs
@@ -8,15 +8,11 @@ public class CameraFollow : MonoBehaviour {
 
 	Vector3 offset;
 
-	// Use this for initialization
 	void Start () {
-		//offset.z = transform.position.z - target.position.z;
 		offset = new Vector3(transform.position.x, transform.position.y - target.position.y, transform.position.z - target.position.z);
-		
 	}
 
 	void LateUpdate () {
-
 		transform.position = target.position + offset;
 	}
 }
--- Assets/Scripts/PlayerTest.cs
+++ Assets/Scripts/PlayerTest.cs
@@ -7,14 +7,33 @@ public class PlayerTest : MonoBehaviour {
 	private Rigidbody rb;
 
 	public float speed;
+	public float steerSpeed;
+
+	bool isTurningRight;
+	bool isTurningLeft;
+	bool isAccelerating;
 
 	void Start () {
-		rb = GetComponent<Rigidbody>();
+		rb = GetComponent<Rigidbody>();	
+	}
+
+	void FixedUpdate () {
+		if (isAccelerating) {
+			rb.AddRelativeForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Acceleration);
+		}
+
+		if (isTurningRight) {
+			rb.AddRelativeTorque(Vector3.up * steerSpeed * Time.deltaTime);
+		}
+
+		if (isTurningLeft) {
+			rb.AddRelativeTorque(-Vector3.up * steerSpeed * Time.deltaTime);
+		}
 	}
 	
 	void Update () {
-		if (Input.GetButton("AButton")) {
-			rb.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Acceleration);
-		}		
+		isTurningRight = Input.GetButton("DPadR");
+		isTurningLeft = Input.GetButton("DPadL");
+		isAccelerating = Input.GetButton("AButton");
 	}
 }
