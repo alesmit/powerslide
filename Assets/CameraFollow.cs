39246d66375e208bf7c08338c80350efbe52bfad
alesmit <alessandro.mitelli@gmail.com>
2018-08-28 10:09:44 +0200

    Initial commit

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+	Vector3 offset;
+
+	// Use this for initialization
+	void Start () {
+		//offset.z = transform.position.z - target.position.z;
+		offset = new Vector3(transform.position.x, transform.position.y - target.position.y, transform.position.z - target.position.z);
+		
+	}
+
+	void LateUpdate () {
+
+		transform.position = target.position + offset;
+	}
+}
---
+++ Assets/Scripts/PlayerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTest : MonoBehaviour {
+
+	private Rigidbody rb;
+
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Throttle")) {
+			Debug.Log("cazzo");
+
+			rb.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Acceleration);
+		}
+		
+	}
+}
