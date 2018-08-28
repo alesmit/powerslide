ccd89870f6c5895b73fe6c3268b122c3cdee019f
Marco Pompa <marcopompa7@gmail.com>
2018-08-28 14:57:59 +0200

    Added controller support

--- Assets/Scripts/PlayerTest.cs
+++ Assets/Scripts/PlayerTest.cs
@@ -8,18 +8,13 @@ public class PlayerTest : MonoBehaviour {
 
 	public float speed;
 
-	// Use this for initialization
 	void Start () {
 		rb = GetComponent<Rigidbody>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		if (Input.GetButton("Throttle")) {
-			Debug.Log("cazzo");
-
+		if (Input.GetButton("AButton")) {
 			rb.AddForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Acceleration);
-		}
-		
+		}		
 	}
 }
