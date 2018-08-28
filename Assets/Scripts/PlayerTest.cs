0a8fc2cd1d0165d710e42bbcf6112cd2016676a4
alesmit <alessandro.mitelli@gmail.com>
2018-08-29 01:18:55 +0200

    Added kart basic movements

--- Assets/Scripts/PlayerTest.cs
+++ Assets/Scripts/PlayerTest.cs
@@ -5,35 +5,61 @@ using UnityEngine;
 public class PlayerTest : MonoBehaviour {
 
 	private Rigidbody rb;
-
+	public Transform centerOfMass;
 	public float speed;
 	public float steerSpeed;
-
 	bool isTurningRight;
 	bool isTurningLeft;
 	bool isAccelerating;
+	public float turningFactor = 1f;
+	float horizontalAxisValue = 0;
+	float throttleValue = 0;
+	public float maxThrottleValue = 100f;
+	public float throttleFactor = 10;
 
 	void Start () {
-		rb = GetComponent<Rigidbody>();	
+		rb = GetComponent<Rigidbody>();
+		rb.centerOfMass = centerOfMass.localPosition;
 	}
 
 	void FixedUpdate () {
-		if (isAccelerating) {
-			rb.AddRelativeForce(Vector3.forward * speed * Time.deltaTime, ForceMode.Acceleration);
-		}
 
-		if (isTurningRight) {
-			rb.AddRelativeTorque(Vector3.up * steerSpeed * Time.deltaTime);
-		}
+		// acceleration
+		rb.MovePosition(transform.position + transform.forward * speed * throttleValue * Time.deltaTime);
+		
 
-		if (isTurningLeft) {
-			rb.AddRelativeTorque(-Vector3.up * steerSpeed * Time.deltaTime);
-		}
+		// steering
+		rb.transform.Rotate(Vector3.up * horizontalAxisValue * steerSpeed * Time.deltaTime);
+
 	}
 	
 	void Update () {
+
 		isTurningRight = Input.GetButton("DPadR");
 		isTurningLeft = Input.GetButton("DPadL");
 		isAccelerating = Input.GetButton("AButton");
+
+		if (isTurningLeft && horizontalAxisValue > -1) {
+			horizontalAxisValue -= Time.deltaTime * turningFactor;
+		} 
+		
+		else if (isTurningRight && horizontalAxisValue < 1) {
+			horizontalAxisValue += Time.deltaTime * turningFactor;
+		} 
+		
+		else if (!isTurningLeft && !isTurningRight) {
+			var factor = horizontalAxisValue > 0 ? -1 : 1;
+			horizontalAxisValue += Time.deltaTime * turningFactor * factor;
+		}
+
+		if (isAccelerating && throttleValue < maxThrottleValue) {
+			throttleValue += Time.deltaTime * throttleFactor;
+
+		} else if (!isAccelerating && throttleValue > 0) {
+			throttleValue -= Time.deltaTime * throttleFactor;
+		}
+
+		Debug.Log("throttle: " + throttleValue);
+
 	}
 }
